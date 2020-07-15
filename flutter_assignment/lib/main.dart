a281ec401a00f9eba98ce10b04f91963f9b096d5
Shnidel <rvarona2016@gmail.com>
2020-07-14 22:22:41 -0500

    AA

--- flutter_assignment/lib/main.dart
+++ flutter_assignment/lib/main.dart
@@ -1,4 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_assignment/src/widgets/my_app.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  String _textToShow = 'La puti va .......';
+
+  void _seeAnswer() {
+    setState(() {
+      _textToShow = 'a hacerme merenguitos quemados mañana, yea !!!!!!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Assigment App'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                _textToShow,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RaisedButton(
+                  color: Colors.blue[300],
+                  child: Text('What what, continue...'),
+                  onPressed: _seeAnswer)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
